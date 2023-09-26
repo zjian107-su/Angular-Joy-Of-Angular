a590f1b8b8266adcb6a25454e24eb369a3af1786
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-09-26 21:34:15 +0100

    :art: comp: filtered city with Subject for debounce

--- src/app/components/filtered-city/filtered-city.component.ts
+++ src/app/components/filtered-city/filtered-city.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import cityList from './data';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-filtered-city',
@@ -10,10 +11,22 @@ export class FilteredCityComponent {
   cityList: string[] = [];
   filteredList: string[] = [];
   filterString: string = '';
+  filterClickSubject: Subject<string> = new Subject<string>();
 
   ngOnInit() {
     this.cityList = cityList;
     this.filteredList = cityList;
+
+    // set up debounce to prevent server request abuse
+    this.filterClickSubject.pipe(debounceTime(1000)).subscribe({
+      next: () => {
+        this.filter();
+      },
+    });
+  }
+
+  onChange(model: string) {
+    this.filterClickSubject.next(model);
   }
 
   filter(): void {
