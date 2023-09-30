4692185af9f3bf5d9453cfe9cbaf56f19b7eda83
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-10-01 00:37:15 +0100

    :art: data: data source - setup

---
+++ src/app/data/index.ts
@@ -0,0 +1,21 @@
+import { User, users } from './users';
+import { Post, posts } from './posts';
+import { Comment, comments } from './comments';
+import { Album, albums } from './albums';
+import { Photo, photos } from './photos';
+import { Todo, todos } from './todos';
+
+export {
+  User,
+  users,
+  Post,
+  posts,
+  Comment,
+  comments,
+  Album,
+  albums,
+  Photo,
+  photos,
+  Todo,
+  todos,
+};
