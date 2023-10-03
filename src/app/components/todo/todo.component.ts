ce9f913136c6a3292e3ad7527e44376055a8bbb0
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-10-03 12:28:49 +0100

    :art: comp: todo - style

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,13 +8,32 @@ import { Todo, todos } from '../../data';
 })
 export class TodoComponent {
   todoItem: Todo = {} as Todo;
-  todoList: Todo[] = [];
+  todoList: Todo[] = [] as Todo[];
 
   ngOnInit() {
     this.todoList = todos.slice(0, 30);
+    let finalList: Todo[] = [] as Todo[];
+    for (let currentTodo of this.todoList) {
+      if (currentTodo.completed) {
+        finalList.push(currentTodo);
+      } else {
+        finalList.unshift(currentTodo);
+      }
+    }
+
+    this.todoList = finalList;
   }
 
   toggleComplete(todo: Todo): void {
     todo.completed = !todo.completed;
+    this.todoList = this.todoList.filter((item) => item !== todo);
+
+    if (todo.completed) {
+      //move bottom when marked as complete
+      this.todoList.push(todo);
+    } else {
+      //move top when marked as incomplete
+      this.todoList.unshift(todo);
+    }
   }
 }
