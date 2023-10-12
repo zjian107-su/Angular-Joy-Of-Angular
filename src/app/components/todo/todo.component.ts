f4e2686eef614a9bde1c417e3ba2741d8c779600
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-10-13 00:23:11 +0100

    :sparkles: feat: todo logic service migration

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,50 +8,16 @@ import { TodoService } from './todo.service';
   styleUrl: './todo.component.css',
 })
 export class TodoComponent {
-  todoItem: Todo = {} as Todo;
-  todoList: Todo[] = [] as Todo[];
-  dataLimit: number = 30;
   todoTitle: string = '';
 
   constructor(public todoService: TodoService) {}
 
   ngOnInit() {
-    this.todoList = todos.slice(0, this.dataLimit);
-    let finalList: Todo[] = [] as Todo[];
-    for (let currentTodo of this.todoList) {
-      if (currentTodo.completed) {
-        finalList.push(currentTodo);
-      } else {
-        finalList.unshift(currentTodo);
-      }
-    }
-
-    this.todoList = finalList;
-    this.todoService.logger('TodoComponent initialized');
-  }
-
-  toggleComplete(todo: Todo): void {
-    todo.completed = !todo.completed;
-    this.todoList = this.todoList.filter((item) => item !== todo);
-
-    if (todo.completed) {
-      //move bottom when marked as complete
-      this.todoList.push(todo);
-    } else {
-      //move top when marked as incomplete
-      this.todoList.unshift(todo);
-    }
+    this.todoService.initializeTodoList();
   }
 
   createTodo(): void {
-    let newTodo: Todo = {
-      userId: 1,
-      id: this.dataLimit + 1,
-      title: this.todoTitle,
-      completed: false,
-    };
-
-    this.todoList.unshift(newTodo);
+    this.todoService.createTodo(this.todoTitle);
     this.todoTitle = '';
   }
 }
--- src/app/components/todo/todo.service.ts
+++ src/app/components/todo/todo.service.ts
@@ -1,12 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Todo, todos } from '../../data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  dataLimit: number = 30;
+  todoList: Todo[] = [] as Todo[];
+
   constructor() {}
+
+  initializeTodoList(): void {
+    let finalList: Todo[] = [] as Todo[];
+    this.todoList = todos.slice(0, this.dataLimit);
+    for (let currentTodo of this.todoList) {
+      if (currentTodo.completed) {
+        finalList.push(currentTodo);
+      } else {
+        finalList.unshift(currentTodo);
+      }
+    }
+
+    this.todoList = finalList;
+    this.logger('TodoComponent initialized');
+  }
 
   logger(message: string): void {
     console.log(message);
+  }
+
+  toggleComplete(todo: Todo): void {
+    todo.completed = !todo.completed;
+    this.todoList = this.todoList.filter((item) => item !== todo);
+
+    if (todo.completed) {
+      //move bottom when marked as complete
+      this.todoList.push(todo);
+    } else {
+      //move top when marked as incomplete
+      this.todoList.unshift(todo);
+    }
+  }
+
+  createTodo(title: string): void {
+    let newTodo: Todo = {
+      userId: 1,
+      id: this.dataLimit + 1,
+      title: title,
+      completed: false,
+    };
+
+    this.todoList.unshift(newTodo);
   }
 }
