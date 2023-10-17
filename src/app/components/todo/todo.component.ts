34625960ccbcf7abdfe0c2b6fa99c132e77e23d9
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-10-17 17:17:32 +0100

    :art: structure: optimization with onPush

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { Todo, todos } from '../../data';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoComponent {
   constructor(public todoService: TodoService) {}
-
 }
