a858aa5b80afa3b444b86de38e0a27cb68070c46
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-10-24 19:06:02 +0100

    :sparkles: feat: fibonacci sequence service migration

--- src/app/components/fibonacci-sequence/fibo.service.ts
+++ src/app/components/fibonacci-sequence/fibo.service.ts
@@ -6,5 +6,15 @@ import { Injectable } from '@angular/core';
 export class FiboService {
   constructor() {}
 
-  fibonacciGen(n: number): void {}
+  fibonacciGen(n: number): number[] {
+    if (n <= 0) {
+      return [];
+    }
+
+    let fibonacci = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+    return fibonacci;
+  }
 }
--- src/app/components/fibonacci-sequence/fibonacci-sequence.component.ts
+++ src/app/components/fibonacci-sequence/fibonacci-sequence.component.ts
@@ -11,28 +11,15 @@ export class FibonacciSequenceComponent {
   inputNum: number = 0;
   hasError: boolean = false;
 
-  constructor(public fiboService: FiboService) {}
+  constructor(private fiboService: FiboService) {}
 
   generateSequence(): void {
-    // clean the array
-    this.hasError = false;
     this.fibonacciSequence = [] as number[];
-
-    // error handling
-    if (this.inputNum === 0 || this.inputNum === null || this.inputNum < 0) {
+    this.hasError = false;
+    if (this.inputNum <= 0) {
       this.hasError = true;
-
-      this.inputNum = 0;
       return;
     }
-
-    // generate fibonacci sequence
-    let fibonacci: number[] = [0, 1];
-
-    for (let i = 2; i <= this.inputNum; i++) {
-      fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
-    }
-
-    this.fibonacciSequence = fibonacci;
+    this.fibonacciSequence = this.fiboService.fibonacciGen(this.inputNum);
   }
 }
