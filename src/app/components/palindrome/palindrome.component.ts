a48188335d8f3adde91371840ff49cd005071d47
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-12-24 22:36:08 +0000

    Revert ":zap: feat: performance improvement with debounce and spinner for better UX"
    This reverts commit 89e83253d108211e408802134eb1aaa874934f80.

--- src/app/components/palindrome/palindrome.component.ts
+++ src/app/components/palindrome/palindrome.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-palindrome',
@@ -8,24 +7,6 @@ import { Subject, debounceTime } from 'rxjs';
 })
 export class PalindromeComponent {
   inputWord: string = '';
-  inputSubject: Subject<string> = new Subject<string>();
-  isWordPalindrome: boolean = false;
-  isLoading: boolean = false;
-
-  ngOnInit() {
-    this.inputSubject.pipe(debounceTime(1000)).subscribe({
-      next: (word: string) => {
-        this.isWordPalindrome = this.isPalindrome(word);
-        this.isLoading = false;
-      },
-      error: (message) => console.log(message),
-    });
-  }
-
-  onChange(model: string) {
-    this.inputSubject.next(model);
-    this.isLoading = true;
-  }
 
   // trim and lowercase the input word to pre-process it
   isPalindrome(word: string): boolean {
